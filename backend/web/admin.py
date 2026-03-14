0cf135c8cd9a1c7402e10ea8b7510c77540b8980
pycharmlover <2783194591@qq.com>
2026-03-14 22:57:19 +0800

    实现好友页面后端

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,4 +1,8 @@
+#每次新写完一个函数，就要在终端backend下执行python manage.py makemigrations 和 python manage.py migrate
+
 from django.contrib import admin
+
+from web.models.friend import Friend
 from web.models.user import UserProfile
 from web.models.character import Character
 
@@ -9,3 +13,7 @@ class UserProfileAdmin(admin.ModelAdmin):
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)
+
+@admin.register(Friend)
+class FriendAdmin(admin.ModelAdmin):
+    raw_id_fields = ('me', 'character',)
